a9b11ed19299ef2901d6fdbe597825da724f75a6
Huguet Hoedric <144238437+Hohugu@users.noreply.github.com>
2026-06-06 18:52:57 +0200

    Create 13_prepare_GEMMA_male_inputs.R

---
+++ 13_prepare_GEMMA_male_inputs.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+
+base <- "/scratch/project_2000886/Hoedric/GWAS_2025"
+
+fam_f <- file.path(base, "PreGWAS/result/GWAS_qc_named.fam")
+pc_f  <- file.path(base, "PreGWAS/result/GWAS_covar_PC5.txt")
+out   <- file.path(base, "GWAS/result")
+
+fam <- read.table(fam_f, stringsAsFactors = FALSE)
+names(fam) <- c("FID","IID","PID","MID","SEX","PHENO")
+
+cov <- read.table(pc_f, header = TRUE, stringsAsFactors = FALSE)
+
+m <- merge(fam[,c("FID","IID")], cov, by=c("FID","IID"), sort=FALSE)
+
+male <- m[m$sex_bin == 1, ]
+
+write.table(
+  male[, c("FID","IID")],
+  file.path(out, "GEMMA_male_keep.txt"),
+  sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE
+)
+
+gemma_covar <- male[, c("pop_bin","PC1","PC2","PC3","PC4","PC5")]
+
+write.table(
+  gemma_covar,
+  file.path(out, "GEMMA_male_covar_PC5.txt"),
+  sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE
+)
+
+cat("Male samples:", nrow(male), "\n")
